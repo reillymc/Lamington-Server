cfe61b4486e7d5a9a396843876020dcf396b4e71
ReillyM-C <reilly@mackenzie-cree.net>
2021-05-30 10:23:04 +1000

    Update Endpoints

--- routes/attachments.js
+++ routes/attachments.js
@@ -2,25 +2,34 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer')
 var path = require('path')
+const { v4: uuidv4 } = require('uuid');
+
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/')
+    destination: function (req, file, callback) {
+        callback(null, 'uploads/')
     },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
+    filename: function (req, file, callback) {
+        callback(null, uuidv4() + path.extname(file.originalname))
     }
 })
 
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg') {
-        cb(null, true)
-    } else {
-        cb(null, true)
+const fileFilter = (req, file, callback) => {
+    const extension = path.extname(file.originalname).toLowerCase();
+    const mimetype = file.mimetype;
+    if (
+        extension !== '.jpg' ||
+        extension !== '.jpeg' ||
+        extension !== '.png' ||
+        mimetype !== 'image/png' ||
+        mimetype !== 'image/jpg' ||
+        mimetype !== 'image/jpeg'
+    ) {
+        callback(null, false, new Error('Incorrect File Type'))
     }
+    callback(null, true)
 }
-var upload = multer({ storage: storage })
+var upload = multer({ storage: storage, fileFilter: fileFilter })
 
 // upload image
 router.post('/upload-image', upload.single('photo'), (req, res) => {
--- routes/meals.js
+++ routes/meals.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 // list all meals
 router.get('/list', function (req, res, next) {
     //get meal ratings and assign ratings as an array on each meal ratings: {user, value}
-    req.db.from('meals').select("id", "name", "recipe", "ingredients", "method", "notes", "photo", "created_by", "times_cooked")
+    req.db.from('meals').select("id", "name", "recipe", "ingredients", "method", "notes", "photo", "created_by", "times_cooked")//.join on ratings and categories
         .then((rows) => {
             res.status(200).json({ "Meals": rows });
         })
@@ -31,7 +31,7 @@ router.get('/meal-roster', function (req, res, next) {
 router.get('/meal-ratings', function (req, res, next) {
     req.db.from('meal_ratings').select("meal_id", "rater_id", "rating")
         .then((rows) => {
-            res.status(200).json({ "years": rows });
+            res.status(200).json({ "ratings": rows });
         })
         .catch((err) => {
             console.log(err);
@@ -45,15 +45,17 @@ router.post('/add-meal', (req, res) => {
         res.status(400).json({ message: `Error updating meals` });
         console.log(`Error on request body:`, JSON.stringify(req.body));
     } else {
+        const { name, recipe, ingredients, method, notes, photo, createdBy } = req.body;
+
         var meal = {
             "id": uuidv4(),
-            "name": req.body.name,
-            "recipe": (req.body.recipe != undefined) ? req.body.recipe : "",
-            "ingredients": (req.body.ingredients != undefined) ? req.body.ingredients : "",
-            "method": (req.body.method != undefined) ? req.body.method : "",
-            "notes": (req.body.notes != undefined) ? req.body.notes : "",
-            "photo": (req.body.photo != undefined) ? req.body.photo : "",
-            "created_by": req.body.createdBy,
+            "name": name,
+            "recipe": (recipe != undefined) ? recipe : "",
+            "ingredients": (ingredients != undefined) ? ingredients : "",
+            "method": (method != undefined) ? method : "",
+            "notes": (notes != undefined) ? notes : "",
+            "photo": (photo != undefined) ? photo : "",
+            "created_by": createdBy,
             "times_cooked": 0
         }
         // database insertion
@@ -63,6 +65,57 @@ router.post('/add-meal', (req, res) => {
             }).catch(error => {
                 res.status(400).json({ message: 'oops! It looks like something went wrong adding your meal :(' });
             })
+    }
+})
+
+// edit a meal
+router.post('/update-meal', (req, res) => {
+    if (!req.body.id || !req.body.name || !req.body.createdBy) {
+        res.status(400).json({ message: `Error updating meals` });
+        console.log(`Error on request body:`, JSON.stringify(req.body));
+    } else {
+        const { id, name, recipe, ingredients, method, notes, photo, createdBy } = req.body;
+
+        // database update
+        req.db('meals').update({ name, recipe, ingredients, method, notes, photo }).where({ id: id })
+            .then(_ => {
+                res.status(201).json({ message: `yay! you've successfully added a meal :)` });
+            }).catch(error => {
+                res.status(400).json({ message: `oops! It looks like something went wrong adding your meal :( ${error}` });
+            })
+    }
+})
+
+
+// rate a meal
+router.post('/rate-meal', (req, res) => {
+    if (!req.body.raterId || !req.body.mealId || !req.body.rating) {
+        res.status(400).json({ message: `You haven't given enough information to rate this meal` });
+        console.log(`Error on request body:`, JSON.stringify(req.body));
+    } else {
+        const { raterId, mealId, rating } = req.body;
+        const prevRatings = req.db.from('meal_ratings').select("meal_id", "rater_id", "rating").where({ 'meal_id': mealId, 'raterId': raterId })
+        var mealRating = {
+            "meal_id": mealId,
+            "rater_id": raterId,
+            "rating": rating,
+        }
+        // database insertion
+        if (prevRatings.rows > 1) { // probably need to await
+            req.db('meal_ratings').update(mealRating)
+                .then(_ => {
+                    res.status(201).json({ message: `yay! you've successfully rated this meal :)` });
+                }).catch(error => {
+                    res.status(400).json({ message: 'oops! It looks like something went wrong rating this meal :(' });
+                })
+        } else {
+            req.db('meal_ratings').insert(mealRating)
+                .then(_ => {
+                    res.status(201).json({ message: `yay! you've successfully rated this meal :)` });
+                }).catch(error => {
+                    res.status(400).json({ message: 'oops! It looks like something went wrong rating this meal :(' });
+                })
+        }
     }
 })
 
