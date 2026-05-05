ea1bdafdbc09f569d379be2152596f8399fd4248
reillymc <reilly@mackenzie-cree.net>
2026-05-05 06:33:32 +0000

    Ingredients plural migration and seed data

--- src/database/knexfile.development.ts
+++ src/database/knexfile.development.ts
@@ -11,7 +11,7 @@ const config: Knex.Config = {
         password: process.env.DB_DEV_PASSWORD,
     },
     seeds: {
-        directory: "./seeds/development",
+        directory: ["./seeds/production", "./seeds/development"],
     },
 };
 
